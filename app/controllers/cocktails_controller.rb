ac77c288575fac5cb5d89c3832c39a802c8295c0
Mary Nazareno <47845955+mgracnazareno@users.noreply.github.com>
2021-04-23 03:06:32 -0400

    Create cocktails_controller.rb
    Create action index for cocktail

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+end
