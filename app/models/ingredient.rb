42297a6c38c5c55fa65b26d85727a60ffad830b3
Mary Nazareno <47845955+mgracnazareno@users.noreply.github.com>
2021-04-23 03:08:19 -0400

    Update ingredient.rb

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 class Ingredient < ApplicationRecord
-    has_many :doses
-    validates :name, presence: true, uniqueness: true
+  has_many :doses
+  validates :name, presence: true, uniqueness: true
 end
