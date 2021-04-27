366d4f7adefc33b080ecf76a157aed4a8fb031b5
Mary Grace Nazareno <mgracenazareno@gmail.com>
2021-04-27 08:40:07 -0400

    Add show action in cocktails

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -5,23 +5,21 @@ class CocktailsController < ApplicationController
     @cocktails = Cocktail.all
   end
 
-  def show
-    @cocktail = Cocktail.find(params[:id])
-  end
   
   def new 
     @cocktail = Cocktail.new
   end
-
   
   def create 
     @cocktail = Cocktail.find(params[:id])
     
-    if @cocktail.save!
+    if @cocktail.save
       redirect_to cocktail_path(@cocktail)
     else 
       render :new
     end
   end
-
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
 end
