8ab28662be458589cf7ca4eddf3a2dcfc02c7eff
Mary Grace Nazareno <mgracenazareno@gmail.com>
2021-05-03 11:12:36 -0400

    Add create and new action for Dose

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,23 +1,33 @@
 class DosesController < ApplicationController
+  before_action :find_cocktail
+
  def new
-  
-   @dose = Dose.new
+  @dose = Dose.new
  end
  
  def create
    @dose = Dose.new(dose_params)
+   @dose.cocktail = @cocktail
    if @dose.save
-     redirect_to @dose_path
+     redirect_to cocktail_path(@cocktail)
    else
      render :new
    end
  end
+
+ def destroy
+  @dose = Dose.find(params[:id])
+  @dose.destroy
+  redirect_to cocktail_path(@dose.cocktail)
+ end
  
  private
 
-  def find_in
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
  def dose_params
-  params.require(:dose).permit(:description)
+  params.require(:dose).permit(:description, :ingredient_id)
  end
 
 end
