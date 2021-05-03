2686a2d7863fdc08b0f6b1d421d104b7078ed710
Mary Grace Nazareno <mgracenazareno@gmail.com>
2021-05-02 22:22:31 -0400

    Add Pages Controller for homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'pages/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get 'cocktails', to: 'cocktails#index'
   # get 'cocktails/new', to: 'cocktails#new', as: :new_cocktail
   # post 'cocktails', to: 'cocktails#create'
   # get 'cocktails/:id', to: 'cocktails#show', as: :cocktail 
+  root to: 'pages#home', as: :root
   resources :cocktails, only: %i[index new create show] do
     resources :doses, only: %i[new create destroy]
   end
